96687fc19f6b4c69a490633c2068e6419334d14b
Sam54000 <44468686+Sam54000@users.noreply.github.com>
2020-07-07 11:11:40 +0200

    Create BigFuknAnalysis.m

---
+++ BigFuknAnalysis.m
@@ -0,0 +1,15 @@
+analysis_path = uigetdir();
+Directory = dir(analysis_path);
+file_names = {Directory.name};
+[Selection, OK] = listdlg('PromptString','Select files to analyse',...
+                      'SelectionMode', 'multiple', ...
+                       'ListString', file_names);
+[nb_files] = size(Selection);
+for n = 1:nb_files(1,2)
+    load(fullfile(Directory(Selection(1,n)).folder,Directory(Selection(1,n)).name));
+    AmplitudesMatAll{n,1} = PostT.Amplitudes;
+end
+
+BigMatrix = cat(2,C{:,1});
+BigStd = std(BigMatri,[],2);
+BigMean = mean(BigMatrix,2);
