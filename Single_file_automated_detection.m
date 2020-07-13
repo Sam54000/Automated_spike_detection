bb30387fee57d29eebf828956bb9aaa3d449bd4a
Marine-Ch <66271056+Marine-Ch@users.noreply.github.com>
2020-07-13 11:01:00 +0200

    Update Single_file_automated_detection.m

--- Single_file_automated_detection.m
+++ Single_file_automated_detection.m
@@ -86,6 +86,7 @@ switch ButtonName
         %% Preparing
         data.d = x.';
         %% spike detection
+        clear labels_BIP idx_spikes qEEG
         [d, DE, discharges, d_decim, envelope,...
         background, envelope_pdf, clustering,...
         labels_BIP, idx_spikes, qEEG(n,:)] = ...
@@ -191,78 +192,4 @@ end
 %                       all_xlsfile{rawpos_center,3} ...
 %                       all_xlsfile{rawpos_center,4}];
 % %xlsread puis loaduntuchnii et convertion mm en voxel puis matcher
-% %le numéro de contact avec les coordonnées, 
-
-%% Calcul de l'écrat-type moyen inter-session
-%clear all 
-analysis_path = uigetdir();
-
-    Directory = dir(analysis_path);
-    file_names = {Directory.name};
-    [Selection, OK] = listdlg('PromptString','Select files to analyse',...
-                      'SelectionMode', 'multiple', ...
-                       'ListString', file_names);
-    [nb_files] = size(Selection);
-    
-    for n = 1:nb_files(1,2)
-        %Load variables from file into workspace
-        load(fullfile(Directory(Selection(1,n)).folder,Directory(Selection(1,n)).name)); %PostT file opening 
-        C{n,1} = PostT.Amplitude.';
-    end
-
-% Création de la Matrice
-    Matrice = cat(2,C{:,1}); %Concatenate arrays along specified dimension, cat(dim, C{:}) for concatenate a cell or structure array containing numeric matrices into a single matrix
-    BigStd = std(Matrice,[],2,'omitnan');
-    BigMean = mean(Matrice,2,'omitnan');
-    
-% Test cohérence matrice BigMean et BigStd
-    Test = zeros(size(BigMean,1),1);
-    Test1 = Test + double(isnan(BigMean));
-    Test2 = Test + double(isnan(BigStd));
-    Test3 = logical(Test1) & logical(Test2);
-    verif1 = sum(double(Test3)-Test1);
-    
-% Supprimer les NaN
-if verif1 == 0
-    BigStd(isnan(BigStd),:)=[];
-    BigMean(isnan(BigMean),:)=[];
-    channelKeep = PostT.Label_Bipolar(~Test3,1);
-else
-    warndlg("attention c'est chelou")
-end
-
-% Répertoire traitement
-BigPostT.Names_channel = channelKeep;
-BigPostT.Big_matrice = Matrice;
-BigPostT.Big_Mean = BigMean;
-BigPostT.Big_Std = BigStd;
-%save(fullfile(saving_folder,['BIGstat' newFname]),'BigPostT');
-
-% Pick up the letters of the electrodes
-nChannels = size(channelKeep,1); %compte le nombre de contacts
-for i = 1:nChannels
-    tmp = channelKeep{i,1}; % Met le nom du contact dans un variable temporaire
-    ChanLettersOnly{i,1} = tmp(isstrprop(tmp,'alpha')); % Prend uniquement les lettres de chaque contacts
-end
-[ElectrodeLetter,indexFirstContact,idx] = unique(ChanLettersOnly,'stable'); % Donne la lettre de chaque electrode (electrode A, electrode B etc...)
-%ElecAffiche(size(idx,1)) = [];
-ElecAffiche(indexFirstContact) = ElectrodeLetter; %Donne le nom de l'electrode Ã  la position souhaitÃ© pour l'affichage dans le XTick label
-
-%Mise en forme
-errorbar(BigMean,BigStd, 'color', [0 0 0]);
-hold on
-plot(BigMean,'r','LineWidth',3);
-xlim([0, size(channelKeep,1)]);
-
-legend('Standard deviation','Mean','Location','southwest')    %Légende
-legend('boxoff')
-title('Ecart-type moyen inter-session') %titre 
-
-xlabel('Electrodes')
-xticks([0:1:size(channelKeep,1)]);
-set(gca,'XTick',1:nChannels);
-set(gca,'XTickLabel',ElecAffiche);
-set(gca, 'fontsize', 12)
-
-ylabel('Amplitude (µV)')
-   
+% %le numéro de contact avec les coordonnées,
